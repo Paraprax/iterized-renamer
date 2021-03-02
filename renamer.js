14b394dbdf808335b849199c3c7d81faf5031dcd
Paraprax <26296581+Paraprax@users.noreply.github.com>
2021-03-01 23:16:38 -0500

    initial functionality with built-in demo array

--- renamer.js
+++ renamer.js
@@ -1,6 +1,16 @@
+files = ['screenshot sdlkafjads;klf', 'screenshot 23543qrf', 'screenshot sadf4325', 'screenshot jk342l;s;klf', 'screenshot 4jkl;ewayz45rtk'];
+
+console.log(files);
+
 const renamer = (array, newname) => {
     /*
         - take in all the files in a directory
-        - rename each to newname plus an incremented number
-    */ 
+    */
+    for (var i = 1; i < array.length; i++) {
+        array[i] = `${newname}-${i}`;
+    }
+    console.log(array);
 }
+
+renamer(files, 'seshead');
+
